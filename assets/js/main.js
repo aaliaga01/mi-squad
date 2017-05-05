0cdf2f4266d70011eb99ea1d2e0b62a5f3c59b93
aaliaga <a.aliaga.val@gmail.com>
2017-05-05 11:58:39 -0300

    nuevo javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-alert("hola mundo")
+function mi-squad(nombre, edad, hobbies){
+ 	this.nombre=nombre;
+ 	this.edad=edad;
+ 	this.hobbies=hobbies;
+}
+
+var cab1=new(gaby, 22, cantar);
+document.write(cab1);
