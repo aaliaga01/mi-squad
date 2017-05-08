c6fb1334ec97ce12f1fcf206557b575c64388f98
aaliaga-casa <a.aliaga.val@gmail.com>
2017-05-07 22:35:19 -0300

    se ve foto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
+
+
 function nuevoMiembro(nombre,edad,hobbie){
+	this.imagen = new Image();
 	this.nombre=nombre;
 	this.edad=edad;
 	this.hobbie=hobbie;
+	//this.like=;
 }
 
 //-----------------------------Se agregan los miembros de mi squad
@@ -22,7 +26,6 @@ var miSquad=[];
 	var patricia = new nuevoMiembro("Patricia Zavala",28,["Cantar"]);
 	miSquad.push(patricia);
 
-
 //----------------------------creando elementos html con DOM
 
 //Referencia al contenedor principal en HTML donde se imprimiran los datos
@@ -40,25 +43,31 @@ miSquad.forEach(function(el){
 			divMiembro.setAttribute("id","integrante");//Se le asigna una clase al div
 			contenedor.appendChild(divMiembro);//se imprime div en html
 
-			var divComent = document.createElement("div");
-			divComent.setAttribute("id","comentario");
-			contenedor.appendChild(divComent);
+			var divComent = document.createElement("div");//se crea div contenedor de comentarios
+			divComent.setAttribute("id","comentario");//se le asigna id y nombre
+			contenedor.appendChild(divComent);//se coloca en el div "contenedor"
 
-			var divTexto= document.createElement("textarea");
-			divTexto.setAttribute("id","comentar");
-			divComent.appendChild(divTexto);
+			var imagen= document.createElement("img");
+			imagen.setAttribute("id","foto");
+			imagen.setAttribute("src","assets/img/img-turis.jpg");
+			divMiembro.appendChild(imagen);
+
+			var divTexto= document.createElement("textarea");//se crea cuadro de texto para comentarios
+			divTexto.setAttribute("id","comentar");//se le da un id y nombre
+			divComent.appendChild(divTexto);//se coloca dentro del div "comentario"
 
 			var boton = document.createElement("button");//Se crea boton
-			boton.setAttribute("id","caja-boton");
-			divTexto.setAttribute("id","comentar");
+			boton.setAttribute("id","caja-boton");//se le da un id y nombre
 			boton.onclick = function(){//con esto funciona al hacer click
- 			alert("hellou");}
-			boton.innerText = "comentar";//texto que mostrara el boton
-			document.getElementById("contenedor").appendChild(boton);//Se indica donde se quiere agregar el boton creado	
+ 			return document.getElementById("comentar").value;}
+			boton.innerText = "enviar";//texto que mostrara el boton
+			document.getElementById("contenedor").appendChild(boton);//Se indica donde se quiere agregar el boton creado
+
+				
 
 		divMiembro.innerHTML += "<p> Nombre: " + el.nombre + "</br>"
 		+ "Edad: " + el.edad + " años" + "</br>" + "Hobbie: " + el.hobbie + "</p>"
-		 + "<hr>"
+		
 		});
 
 
